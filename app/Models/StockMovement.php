63f4753fce218a5876fab2460e4ebeff46877889
shazeedul <syedshazeedul@gmail.com>
2025-05-24 00:05:10 +0600

    add stock movement

---
+++ app/Models/StockMovement.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StockMovement extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'product_variant_item_id',
+        'type',
+        'quantity',
+        'note',
+    ];
+}
